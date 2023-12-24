3c5c75ed70e7122a1c9e7f4731808f595b3f5c2c
My Name <myEmail@example.com>
2023-12-25 01:05:12 +0300

    Добавьте файлы проекта.

---
+++ ChatClient/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string host = "127.0.0.1";
+        int port = 8080;
+        using TcpClient client = new TcpClient();
+        StreamReader? reader = null;
+        StreamWriter? writer = null;
+
+        try
+        {
+            await client.ConnectAsync(host, port);
+            reader = new StreamReader(client.GetStream());
+            writer = new StreamWriter(client.GetStream());
+            if (writer is null || reader is null) return;
+
+            Console.Write("Введите свое имя: ");
+            string? userName = Console.ReadLine();
+            Console.WriteLine("Вы ыошли в сеть");
+            Console.WriteLine($"Что бы отправить сообщение пользователю пропишите: /message (Id собеседника) свое сообщение");
+            Console.WriteLine($"Что бы отправить всем, напишите и отправьте сообщение без id человека )");
+
+            await writer.WriteLineAsync(userName);
+            await writer.FlushAsync();
+
+            Task.Run(() => ReceiveMessageAsync(reader));
+
+            while (true)
+            {
+                string? message = Console.ReadLine();
+
+                if (message != null && message.StartsWith("/message"))
+                {
+                    string[] parts = message.Split(' ');
+                    if (parts.Length >= 3)
+                    {
+                        await writer.WriteLineAsync(message);
+                        await writer.FlushAsync();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неправильный формат отправленного сообщения, используйте: /message (Id собеседника) свое сообщение");
+                    }
+                }
+                else
+                {
+                    await writer.WriteLineAsync(message);
+                    await writer.FlushAsync();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            writer?.Close();
+            reader?.Close();
+        }
+    }
+
+    static async Task ReceiveMessageAsync(StreamReader reader)
+    {
+        while (true)
+        {
+            try
+            {
+                string? message = await reader.ReadLineAsync();
+                if (string.IsNullOrEmpty(message)) continue;
+                Print(message);
+            }
+            catch
+            {
+                break;
+            }
+        }
+    }
+
+    static void Print(string message)
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            var position = Console.GetCursorPosition();
+            int left = position.Left;
+            int top = position.Top;
+            Console.MoveBufferArea(0, top, left, 1, 0, top + 1);
+            Console.SetCursorPosition(0, top);
+            Console.WriteLine(message);
+            Console.SetCursorPosition(left, top + 1);
+        }
+        else Console.WriteLine(message);
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Threading.Tasks;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        ServerObject server = new ServerObject();
+        await server.ListenAsync();
+    }
+}
+class ServerObject
+{
+    TcpListener tcpListener = new TcpListener(IPAddress.Any, 8080);
+    List<ClientObject> clients = new List<ClientObject>();
+    protected internal Dictionary<int, string> clientUsernames = new Dictionary<int, string>();
+    int clientIdCounter = 1;
+
+    protected internal void RemoveConnection(int id)
+    {
+        ClientObject client = clients.FirstOrDefault(c => c.Id == id);
+        if (client != null)
+        {
+            clients.Remove(client);
+            client.Close();
+        }
+    }
+
+    protected internal async Task ListenAsync()
+    {
+        try
+        {
+            tcpListener.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+            while (true)
+            {
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+
+                ClientObject clientObject = new ClientObject(tcpClient, clientIdCounter++, this);
+                clients.Add(clientObject);
+                Task.Run(clientObject.ProcessAsync);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Disconnect();
+        }
+    }
+
+    protected internal async Task BroadcastMessageAsync(string message, int senderId, int? receiverId = null)
+    {
+        foreach (var client in clients)
+        {
+            if (client.Id == senderId)
+            {
+                continue;
+            }
+
+            if (receiverId != null && client.Id != receiverId)
+            {
+                continue;
+            }
+
+            await client.Writer.WriteLineAsync(message);
+            await client.Writer.FlushAsync();
+        }
+    }
+
+    protected internal void Disconnect()
+    {
+        foreach (var client in clients)
+        {
+            client.Close();
+        }
+        tcpListener.Stop();
+    }
+}
+
+class ClientObject
+{
+    protected internal int Id { get; }
+    protected internal StreamWriter Writer { get; }
+    protected internal StreamReader Reader { get; }
+
+    TcpClient client;
+    ServerObject server;
+
+    public ClientObject(TcpClient tcpClient, int clientId, ServerObject serverObject)
+    {
+        client = tcpClient;
+        Id = clientId;
+        server = serverObject;
+        var stream = client.GetStream();
+        Reader = new StreamReader(stream);
+        Writer = new StreamWriter(stream);
+    }
+
+    public async Task ProcessAsync()
+    {
+        try
+        {
+            string? userName = await Reader.ReadLineAsync();
+            server.clientUsernames.Add(Id, userName);
+
+            string? message = $"[{Id}] | {userName} вошел в чат";
+            await server.BroadcastMessageAsync(message, Id);
+            Console.WriteLine(message);
+
+            while (true)
+            {
+                try
+                {
+                    message = await Reader.ReadLineAsync();
+                    if (message == null) continue;
+
+                    if (message.StartsWith("/message"))
+                    {
+                        string[] parts = message.Split(' ');
+                        if (parts.Length >= 3)
+                        {
+                            int targetUserId = int.Parse(parts[1]);
+                            message = $"[{Id}] | {userName} (личное сообщение): {string.Join(' ', parts.Skip(2))}";
+                            await server.BroadcastMessageAsync(message, Id, targetUserId);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        message = $"[{Id}] | {userName}: {message}";
+                        await server.BroadcastMessageAsync(message, Id);
+                    }
+                }
+                catch
+                {
+                    message = $"[{Id}] | {userName} покинул чат";
+                    Console.WriteLine(message);
+                    await server.BroadcastMessageAsync(message, Id);
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            server.RemoveConnection(Id);
+        }
+    }
+
+    protected internal void Close()
+    {
+        Writer.Close();
+        Reader.Close();
+        client.Close();
+    }
+}
